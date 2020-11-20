5fcb5ea548627832ab808ff3ce555e946c83ffab
vroddon <vrodriguez@fi.upm.es>
2020-11-20 17:01:47 +0000

    quito 127.0.0.1

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo /usr/bin/java -jar /ixasrl/target/ixasrl-1.0.jar --server.port=8092 --server.use-forward-headers=true --server.address=127.0.0.1 --server.servlet.context-path=/ixasrl
+sudo /usr/bin/java -jar /ixasrl/target/ixasrl-1.0.jar --server.port=8092 --server.use-forward-headers=true --server.servlet.context-path=/ixasrl
